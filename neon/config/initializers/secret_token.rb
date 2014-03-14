9944bec3f1d41d2a64f5c6d714f3daf8958e7065
Sirui Sun <sirui.sun1@gmail.com>
2014-03-13 23:32:33 -0700

    Initialize repo

---
+++ neon/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Neon::Application.config.secret_key_base = '35f27e037adf99a112866beaa7b4088e180c6ee2503b9d2abd9992901837665f33f48b636690e44972bfb0b4ea0e5602f3df93d8077535c3e814a3ac7de21d27'
